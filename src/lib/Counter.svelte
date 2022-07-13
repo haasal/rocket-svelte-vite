67005a2d75bfcfe4092d50df0f1aaa185eef8634
Alexander Haas <mail@haas-alexander.com>
2022-07-13 19:51:05 +0200

    feat: :poop: I added some tailwind classes to the button
    The page now looks super ugly but thats not the point

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
+    let count: number = 0;
+    const increment = () => {
+        count += 1;
+    };
 </script>
 
-<button on:click={increment}>
-  count is {count}
+<button
+    on:click={increment}
+    class="rounded bg-slate-200 hover:bg-slate-300 m-2 py-1 w-32 transition-all"
+>
+    count is {count}
 </button>
